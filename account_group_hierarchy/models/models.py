a76431f7e4eb1d274d21bced1c7d1ed8b64e397b
Aneesh AV <aneesh@amcde.com>
2022-04-17 14:27:05 +0300

    [IMP]Group update

--- account_group_hierarchy/models/models.py
+++ account_group_hierarchy/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
 # class addson/account_parent(models.Model):
@@ -16,3 +16,26 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+class AccountGroup(models.Model):
+    _inherit = 'account.group'
+
+    @api.constrains('code_prefix_start', 'code_prefix_end')
+    def _constraint_prefix_overlap(self):
+        self.env['account.group'].flush()
+        query = """
+                SELECT other.id FROM account_group this
+                JOIN account_group other
+                  ON char_length(other.code_prefix_start) = char_length(this.code_prefix_start)
+                 AND other.id != this.id
+                 AND other.company_id = this.company_id
+                 AND (
+                    other.code_prefix_start <= this.code_prefix_start AND this.code_prefix_start <= other.code_prefix_end
+                    OR
+                    other.code_prefix_start >= this.code_prefix_start AND this.code_prefix_end >= other.code_prefix_start
+                )
+                WHERE this.id IN %(ids)s
+            """
+        self.env.cr.execute(query, {'ids': tuple(self.ids)})
+        res = self.env.cr.fetchall()
+        # if res:
+        #     raise ValidationError(_('Account Groups with the same granularity can\'t overlap'))
